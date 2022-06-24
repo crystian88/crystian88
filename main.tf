7c5cbe2c9d8adbb6aec7e526cf7f78f1c589f7a5
crystian88 <43910937+crystian88@users.noreply.github.com>
2022-06-24 15:34:10 +0200

    Add files via upload

--- main.tf
+++ main.tf
@@ -9,9 +9,28 @@ provider "aws" {
 resource "aws_instance" "EC2instance"{
   ami = "ami-0fb653ca2d3203ac1" #this is the image you can create with docker
   instance_type = "t2.micro" 
+  vpc_security_group_ids = [aws_security_group.instance.id] #it tells to apply the policy
+  
+  user_data = <<-EOF    #EOF are used to create multiline instead of inserting \n
+              #!/bin/bash
+			  echo "Hello, World" > index.html
+			  nohup busybox httpd -f -p 8080 &
+			  EOF
   
   tags{
     Name = "nameoftheEC2"
   }
 }
+
+resource "aws_security_group" "Instance"{
+  name = "security-group-for-instance"
+  
+  ingress{
+    from_port = 8080
+	to_port = 8080
+	protocol = "tcp"
+	cidr_blocks = ["0.0.0.0/0"] #all IP addresses
+  }
+}
+
 
